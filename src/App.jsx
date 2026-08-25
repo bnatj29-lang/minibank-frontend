e0f7524ac2bb180611e9cb479975b58a704c2d56
Bruna da Silva <b.natj29@gmail.com>
2026-08-25 12:14:10 -0300

    integração do login + início do roteamento.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import LoginForm from "./components/LoginForm";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
     return (
         <div style={{ textAlign: "center", marginTop: 40 }}>
-            <LoginForm />
+            <Routes>
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/home" element={<Home />} />
+            </Routes>
         </div>
     );
 }
