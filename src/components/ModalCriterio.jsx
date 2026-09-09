3adc2856f38bfee6f7c79714c0c41c80419e5ad7
Rafaela Fernandes <rafaela63466666@alunos.sc.senac.br>
2026-09-09 14:52:59 -0300

    criacao do modal de criterios/missoes

---
+++ src/components/ModalCriterio.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+
+function ModalCriterio({ isOpen, onClose, onCriar }) {
+    const [criterio, setCriterio] = useState(""); //guarda o valor do que vc digita
+
+    if (!isOpen) {
+        return null;
+    }
+
+    const handleCriar = () => {
+        if (!criterio.trim()) {
+            return;
+        }
+
+        onCriar(criterio); //cria
+        setCriterio(""); //atribui valor ao criterio (??)
+        onClose(); //fecha o modal
+    };
+
+    return (
+        <div className="modal d-block" tabIndex="-1">
+            <div className="modal-dialog">
+                <div className="modal-content">
+
+                    <div className="modal-header">
+                        <h5 className="modal-title">
+                            Nova Missão
+                        </h5>
+
+                        <button
+                            type="button"
+                            className="btn-close"
+                            onClick={onClose}
+                        ></button>
+                    </div>
+
+                    <div className="modal-body">
+                        <label className="form-label">
+                            Nome da missão *
+                        </label>
+
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Ex: Arrumar o quarto, Fazer as tarefas..."
+                            value={criterio}
+                            onChange={(e) => setCriterio(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="modal-footer">
+                        <button
+                            type="button"
+                            className="btn btn-secondary"
+                            onClick={onClose}
+                        >
+                            Cancelar
+                        </button>
+
+                        <button
+                            type="button"
+                            className="btn btn-success"
+                            onClick={handleCriar}
+                        >
+                            Criar
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ModalCriterio;
