84042bee8af2ff9300eee48fa48081829ef6c235
Rafaela Fernandes <rafaela63466666@alunos.sc.senac.br>
2026-09-09 13:53:29 -0300

    criação do arquivo Missoes.jsx

---
+++ src/pages/Missoes.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function  Missoes() {
+    return (
+        <div>
+            <h1>Missões</h1>
+
+            <p>
+                Atribua notas de 0 a 10 para cada critério
+            </p>
+
+
+
+
+        </div>
+    );
+}
+export default Missoes;
